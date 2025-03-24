d76866fc1ff43abd28728e5a6efa8bec8ab00e1a
Sophia Eggert <fifieggert@gmail.com>
2025-03-24 19:01:45 -0300

    update requests

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -27,16 +27,24 @@ const userController = {
   },
 
   update: (req: Request, res: Response) => {
-    const { ID_usuario } = req.params
-    const { nome, telefone, email } = req.body;
-    UserRepository.update(nome, telefone, email, ID_usuario, (err, result) => {
-      if (err) return res.status(500).json({ error: "Erro no servidor" });
+    const ID_usuario = Number(req.params.ID_usuario);
+    const { nome, telefone, email, cpf } = req.body;
+    console.log("ID_usuario recebido:", ID_usuario);
+    console.log("Dados recebidos:", { nome, telefone, email, cpf });
+
+    UserRepository.update(ID_usuario, nome, telefone, email, (err, result) => {
+      if (err) {
+          console.error("Erro no update:", err);
+          return res.status(500).json({ error: "Erro no servidor", details: err });
+      }
       res.status(200).json({ message: "Usuário atualizado com sucesso", result });
-    });
+  });
+  
   },
 
   delete: (req: Request, res: Response) => {
     const { ID_usuario } = req.params;
+    console.log(ID_usuario, 'teste')
     UserRepository.delete(ID_usuario, (err, result) => {
       if (err) return res.status(500).json({ error: "Erro no servidor" });
       res.status(200).json({ message: "Usuário deletado com sucesso", result });
